379055706d5c0a8c9267054c60156a8431be339a
veos-sxarr <veos@sxarr.jp.nec.com>
2019-01-18 09:18:14 +0900

    Updated the document of libsysve to v2.0.1

--- modules.js
+++ modules.js
@@ -1,6 +1,7 @@
 var modules =
 [
-    [ "VE DMA (EXPERIMENTAL)", "group__vedma.html", "group__vedma" ],
+    [ "VE DMA", "group__vedma.html", "group__vedma" ],
+    [ "Accelerated I/O", "group__veaccio.html", null ],
     [ "VE AIO", "group__veaio.html", "group__veaio" ],
     [ "VH call", "group__vhcall.html", "group__vhcall" ],
     [ "VH-VE SHM", "group__vhshm.html", "group__vhshm" ]
